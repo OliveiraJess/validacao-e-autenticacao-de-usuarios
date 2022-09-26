1c6b9fcfa4512944f216fac2dde5e3635487e224
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-09-26 20:37:39 -0300

    templete basico react

---
+++ src/pages/Main/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      {/* <img
+        src=""
+        height="256"
+        alt="Dev Samurai"
+      /> */}
+      <h1>Título!</h1>
+      <p>Template básico.</p>
+    </Container>
+  );
+}
+
+export default Main;
