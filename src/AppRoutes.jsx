a1d48b0936256e66aed7cde91ae54f714cedcc06
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-09-30 17:37:27 -0300

    criando o AuthProvider

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,29 +2,18 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/login/LoginPage';
-import { AuthContext } from "./auth/auth";
+import { AuthProvider } from "./auth/auth";
 
 const AppRoutes = () => {
 
-    const [user, setUser] = useState(null);
-
-    const login = (email, password) => {
-        console.log("login auth", { email, password });
-        setUser({ id: "123", email });
-    };
-
-    const logout = () => {
-        console.log("logout");
-    };
-
     return (
         <Router>
-            <AuthContext.Provider value={{ authenticated: !!user, user, login, logout }}>
+            <AuthProvider>
                 <Routes>
                     <Route exact path="/login" element={<LoginPage />} />
                     <Route exact path="/" element={<HomePage />} />
                 </Routes>
-            </AuthContext.Provider>
+            </AuthProvider>
         </Router>
     )
 }
