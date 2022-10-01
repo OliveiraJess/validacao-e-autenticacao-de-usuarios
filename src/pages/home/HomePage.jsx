73ddc7cb27f3d15581d144592c2ea436e6858ff6
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-09-30 23:04:52 -0300

    css page home

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,19 +3,29 @@ import { AuthContext } from "../../auth/auth";
 import "../home/homePage.css"
 
 const HomePage = () => {
-    const {authenticated, logout } = useContext(AuthContext);
+    const { authenticated, logout } = useContext(AuthContext);
 
     const handleLogout = () => {
         logout()
     }
 
+    const handleBack = () => {
+        window.history.back();
+    }
+
     return (
         <div className="container">
+            <div className="header"></div>
             <h1 className="title">Home Page</h1>
+            <div className="card" ></div>
+            <div className="cardButton">
+                <button className="buttonLogout" onClick={handleLogout}>Logout</button>
+                <button className="buttonBack" onClick={handleBack}>Back</button>
+            </div>
             {/* <p>
                 {String(authenticated)}
             </p> */}
-            <button className="buttonLogout" onClick={handleLogout}>Logout</button>
+
         </div>
 
     );
